ebd4a054c716193a7728387b038fa33d33ec84c8
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-08-07 11:52:31 +0545

    day 2 started video #3 30:00 complete

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class AdminLoginController extends Controller
 {
     public function index(){
         return view('admin.login');
+    }
+
+    public function authenticate(REQUEST $request){
+        $validator = Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+        if($validator->passes()){
+
+        }else{
+            return redirect()->route('admin.login')->withErrors($validator)->withInput($request->only('email'));
+        }
     }
 }
